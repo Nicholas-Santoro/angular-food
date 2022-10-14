8db50be7650158d9c074cca03821514cdc4ade3e
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-10-14 16:41:41 +0200

    feat:food service

---
+++ src/app/services/food/food.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll():String[]{
+    return [
+      '/assets/image/foods/aglio-olio-peperoncino.jpg',
+      '/assets/image/foods/carbonara.jpg',
+      '/assets/image/foods/zuppa-patate-cipolle.jpg',
+      '/assets/image/foods/hamburger.jpg',
+      '/assets/image/foods/filetto-manzo.jpg',
+      '/assets/image/foods/fiorentina.jpg',
+      '/assets/image/foods/patatine.jpg',
+      
+    ]
+  }
+}
