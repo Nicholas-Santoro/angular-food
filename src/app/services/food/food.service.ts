475fe3112036caa6d02dc933a76b73643488e762
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-10-14 18:00:50 +0200

    feat: inserimento descrizione

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Food } from 'src/app/shared/models/food';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,61 @@ export class FoodService {
 
   constructor() { }
 
-  getAll():String[]{
+  getAll():Food[]{
     return [
-      '/assets/image/foods/aglio-olio.jpg',
-      '/assets/image/foods/carbonara.jpg',
-      '/assets/image/foods/hamburger.jpg',
-      '/assets/image/foods/filetto-manzo.jpg',
-      '/assets/image/foods/fiorentina.jpg',
-      '/assets/image/foods/patatine.jpg',
+      {
+        id:1,
+        name:'Pasta aglio olio e peperoncino',
+        cookTime:'10',
+        price: 9,
+        imageUrl:'/assets/image/foods/aglio-olio.jpg',
+        tags:['pasta', 'pranzo']
+      },
+      {
+        id:2,
+        name:'Pasta carbonara',
+        cookTime:'25',
+        price: 11,
+        imageUrl:'/assets/image/foods/carbonara.jpg',
+        tags:['pasta', 'pranzo']
+      },
+      {
+        id:3,
+        name:'Filetto di manzo',
+        cookTime:'10',
+        price: 17,
+        imageUrl:'/assets/image/foods/filetto-manzo.jpg',
+        tags:['pranzo']
+      },
+      {
+        id:4,
+        name:'Fiorentina',
+        cookTime:'15-20',
+        price: 25,
+        imageUrl:'/assets/image/foods/fiorentina.jpg',
+        tags:[ 'pranzo']
+      },
+      {
+        id:5,
+        name:'Hamburger',
+        cookTime:'40-55',
+        price: 7,
+        imageUrl:'/assets/image/foods/hamburger.jpg',
+        tags:['hamburger', 'pranzo']
+      },
+      {
+        id:6,
+        name:'Patatine fritte',
+        cookTime:'25',
+        price: 5,
+        imageUrl:'/assets/image/foods/patatine.jpg',
+        tags:['fast-food', 'fritto']
+      },
+      
+     
+      
+      
+      
       
     ]
   }
---
+++ src/app/shared/models/food.ts
@@ -0,0 +1,8 @@
+export class Food{
+    id!:number;
+    name!:string;
+    price!:number;
+    tags?:string[];
+    imageUrl!:string;
+    cookTime!:string;
+}
