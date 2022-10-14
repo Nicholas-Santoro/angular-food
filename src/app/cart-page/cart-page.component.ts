558bb8361bff8dde74583f102533f02a523aca4d
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-10-14 19:11:02 +0200

    feat: bottone per carrello

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart/cart.service';
+import { Cart } from '../shared/models/Carts';
+import { CartItem } from '../shared/models/CartItem';
 
 @Component({
   selector: 'app-cart-page',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPageComponent implements OnInit {
 
-  constructor() { }
+  cart!:Cart;
+  constructor(private cartService: CartService) { }
+
+
 
   ngOnInit(): void {
   }
 
+  removeFromCart(cartItem:CartItem){
+    this.cartService.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItem:CartItem, quantityInString:string){
+    const quantity= parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+    this.setCart();
+  }
+
+  setCart(){
+    this.cart = this.cartService.getCart();
+  }
 }
